edaff4e20bd0e1d0b301630128774fa9ea96ff4a
Abhi trivedi <abhi.t@crestinfosystems.com>
2024-02-16 17:12:15 +0530

    Update book feature added

--- src/controller/book.controller.ts
+++ src/controller/book.controller.ts
@@ -7,6 +7,8 @@ import Book from "../models/book.model";
  * @param res books []
  * @param next error
  * @description To get all books
+ *
+ * @todo ADD sorting feature
  */
 export async function getAllBooks(
   req: Request,
@@ -63,6 +65,69 @@ export async function getBookById(
     if (!book) {
       throw new Error("No book found");
     }
+    res.json(book);
+  } catch (err) {
+    next(err);
+  }
+}
+
+/**
+ *
+ * @param req params id
+ * @param res No Content
+ * @param next error
+ * @description To delete book by id
+ * @async
+ * @param id
+ */
+export async function deleteBookById(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    await Book.deleteOne({ _id: req.params.id });
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+}
+
+/**
+ *
+ * @param req
+ * @param res
+ * @param next
+ * @description To update book model
+ * @async
+ */
+export async function updateBookById(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const updateAllowed = ["author", "title", "genre", "publicationYear"];
+  const updateBody = Object.keys(req.body);
+
+  const isUpdateAllowed = updateBody.every((update) =>
+    updateAllowed.includes(update)
+  );
+  try {
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed for following fields.");
+    }
+
+    const book = await Book.findById(req.params.id)!;
+    if (!book) {
+      throw new Error("No Book Found.");
+    }
+
+    updateBody.forEach((update) => {
+      (book as any)[update] = req.body[update];
+    });
+
+    await book?.save();
+
     res.json(book);
   } catch (err) {
     next(err);
--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -2,14 +2,20 @@ import { Router } from "express";
 
 import {
   addBook,
+  deleteBookById,
   getAllBooks,
   getBookById,
+  updateBookById,
 } from "../controller/book.controller";
 
 const router = Router();
 
 router.route("/").get(getAllBooks).post(addBook);
 
-router.route("/:id").get(getBookById);
+router
+  .route("/:id")
+  .get(getBookById)
+  .delete(deleteBookById)
+  .patch(updateBookById);
 
 export default router;
