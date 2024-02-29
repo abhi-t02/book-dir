4192fae8e696ca1ce324de4ce4bc28137ba3927b
Abhi trivedi <abhi.t@crestinfosystems.com>
2024-02-29 09:15:22 +0530

    code refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,29 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
 
 import morganMiddleware from "./middleware/logger.middleware";
-import { bookRouter, userRouter } from "./routes";
+import appRouter from "./routes";
 
 import CustomError from "./types/error.types";
 
 const app = express();
+dotenv.config();
 
-// CORS configuration
+// CORS configuration and headers configuration
 app.use(cors());
+app.use(helmet());
 
 // middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 
 // logger
 app.use(morganMiddleware);
 
 // Routes
-app.use("/api/users", userRouter);
-app.use("/api/books", bookRouter);
+app.use(appRouter);
 
 // error handler
 app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,12 @@
+import { Router } from "express";
+
 import userRouter from "./user.route";
 import bookRouter from "./book.route";
 
-export { userRouter, bookRouter };
+const router = Router();
+const baseUrl = `/api/v1`;
+
+router.use(`${baseUrl}/users`, userRouter);
+router.use(`${baseUrl}/books`, bookRouter);
+
+export default router;
